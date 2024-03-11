69c91003f702e3a6e967da739bb3b99b8dca9c4c
XavierCS-dev <xavierelwell@gmail.com>
2024-03-11 16:14:07 +0000

    Implement basic transformations

--- src/engine/transform/transform.rs
+++ src/engine/transform/transform.rs
@@ -1,13 +1,28 @@
-use crate::engine::primitives::matrix::Matrix4;
+use crate::engine::primitives::{matrix::Matrix4, vector::Vector3};
 
 pub struct Transform2D {
     matrix: Matrix4,
+    rotation: f32,
+    scale: f32,
+    position: Vector3,
 }
 
 impl Transform2D {
     pub fn new() -> Self {
         let matrix = Matrix4::new();
-        Self { matrix }
+        let rotation = 0.0;
+        let scale = 1.0;
+        let position = Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        Self {
+            matrix,
+            rotation,
+            scale,
+            position,
+        }
     }
 
     pub fn to_raw(&self) -> [[f32; 4]; 4] {
@@ -16,3 +31,26 @@ impl Transform2D {
 }
 
 pub struct Transform2DSystem;
+
+impl Transform2DSystem {
+    pub fn rotate(transform: &mut Transform2D, radians: f32) {
+        transform.rotation = radians;
+        transform.matrix.inner[0][0] = radians.cos() * transform.scale;
+        transform.matrix.inner[0][1] = radians.sin();
+        transform.matrix.inner[1][0] = -(radians.sin());
+        transform.matrix.inner[1][1] = radians.cos() * transform.scale;
+    }
+
+    pub fn translate(transform: &mut Transform2D, position: Vector3) {
+        transform.position = position;
+        transform.matrix.inner[3][0] = position.x;
+        transform.matrix.inner[3][1] = position.y;
+        transform.matrix.inner[3][2] = position.z;
+    }
+
+    pub fn scale(transform: &mut Transform2D, scale: f32) {
+        transform.scale = scale;
+        transform.matrix.inner[0][0] = transform.rotation.cos() * scale;
+        transform.matrix.inner[1][1] = transform.rotation.cos() * scale;
+    }
+}
