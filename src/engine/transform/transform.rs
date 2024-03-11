2885e5d60ae70c1468e2bb36fa2ba61380dde614
XavierCS-dev <xavierelwell@gmail.com>
2024-03-11 15:58:32 +0000

    Add basic initialisation for Matrix4 and Transform2D

--- src/engine/transform/transform.rs
+++ src/engine/transform/transform.rs
@@ -1,1 +1,18 @@
-pub struct Transform2D {}
+use crate::engine::primitives::matrix::Matrix4;
+
+pub struct Transform2D {
+    matrix: Matrix4,
+}
+
+impl Transform2D {
+    pub fn new() -> Self {
+        let matrix = Matrix4::new();
+        Self { matrix }
+    }
+
+    pub fn to_raw(&self) -> [[f32; 4]; 4] {
+        self.matrix.inner
+    }
+}
+
+pub struct Transform2DSystem;
