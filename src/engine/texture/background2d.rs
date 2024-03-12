158c33c05645a19edd4bb27f8197a073a52414a1
XavierCS-dev <xavierelwell@gmail.com>
2024-03-12 12:28:02 +0000

    Add Background2D

---
+++ src/engine/texture/background2d.rs
@@ -0,0 +1,8 @@
+// Render before layers.
+// Support parallax in the future.
+// should just request bind groups etc and should just work
+// including any animations etc
+pub struct Background2D;
+
+// manage all background related operations
+pub struct Background2DSystem;
--- src/engine/texture/mod.rs
+++ src/engine/texture/mod.rs
@@ -1,2 +1,3 @@
+pub mod background2d;
 pub mod texture2d;
 pub mod texture_atlas2d;
